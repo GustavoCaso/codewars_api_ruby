7431f9a82c7a505b0f86b1340fceebd40ed8a22d
GustavoCaso <gustavocaso@gmail.com>
2015-12-23 23:16:04 +0100

    Added configuration class

---
+++ lib/codewars_api_ruby/configuration.rb
@@ -0,0 +1,24 @@
+module CodewarsApiRuby
+  module Configuration
+    PUBLIC_METHODS = [:api_key]
+
+    API_VERSION = 'v1'
+    DEFAULT_ENDPOINT = "https://www.codewars.com/api/v1"
+
+    API_KEY_DEFAULT = nil
+
+    attr_accessor *PUBLIC_METHODS
+
+    def self.extended(base)
+      base.reset
+    end
+
+    def reset
+      self.api_key = API_KEY_DEFAULT
+    end
+
+    def configure
+      yield self
+    end
+  end
+end
