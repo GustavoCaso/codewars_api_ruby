a945c1eb881aef5bc6c4e42ac002df8e0a1b18b7
GustavoCaso <gustavocaso@gmail.com>
2015-12-24 01:29:36 +0100

    Add User

--- lib/codewars_api_ruby.rb
+++ lib/codewars_api_ruby.rb
@@ -1,3 +1,4 @@
+require "codewars_api_ruby/user"
 require "codewars_api_ruby/client"
 require "codewars_api_ruby/configuration"
 require "codewars_api_ruby/version"
@@ -5,4 +6,9 @@ require "codewars_api_ruby/version"
 module CodewarsApiRuby
   extend Configuration
   extend Client
+
+  def self.user(username_or_id:)
+    status, response = get(path: User::PATH, params: username_or_id)
+    User.new(data: response)
+  end
 end
