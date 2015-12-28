3d290402a9f0b539cf67f2b08bb5c69b8db934c4
GustavoCaso <gustavocaso@gmail.com>
2015-12-28 11:49:57 +0100

    Added Object Builder helper

--- lib/codewars_api_ruby/next_kata.rb
+++ lib/codewars_api_ruby/next_kata.rb
@@ -1,49 +1,12 @@
-require 'json'
+require 'codewars_api_ruby/object_builder'
+
 module CodewarsApiRuby
   class NextKata
+    include ObjectBuilder
     PATH = 'code-challenges'
 
     def initialize(data:)
-      next_kata_data = JSON.parse(data)
-      _build_next_kata(next_kata_data)
-    end
-
-    private
-
-    def _build_next_kata(data)
-      data.each do |key, value|
-        if value.is_a? Hash
-          _build_methods(value)
-        else
-          _mount_values_and_methods(key, value)
-        end
-      end
-    end
-
-    def _build_methods(values)
-      values.each do |key, value|
-        _mount_values_and_methods(key, value)
-      end
-    end
-
-    def _mount_values_and_methods(key, value)
-      new_key = _underscore_key(key)
-      _build_attr_reader(new_key)
-      _set_attr_value(new_key, value)
-    end
-
-    def _build_attr_reader(key)
-      self.class.instance_eval("attr_reader :#{key}")
-    end
-
-    def _set_attr_value(key, value)
-      instance_variable_set("@#{key}", value)
-    end
-
-    def _underscore_key(key)
-      match = key.match(/^([a-z]*)([A-Z].*)/)
-      return key unless match
-      "#{match[1]}_#{match[2].downcase}"
+      _build_object(data)
     end
   end
 end
--- lib/codewars_api_ruby/user.rb
+++ lib/codewars_api_ruby/user.rb
@@ -1,11 +1,12 @@
-require 'json'
+require 'codewars_api_ruby/object_builder'
+
 module CodewarsApiRuby
   class User
+    include ObjectBuilder
     PATH = 'users'
 
     def initialize(data:)
-      user_data = JSON.parse(data)
-      _build_user(user_data)
+      _build_object(data)
       _build_language_methods
     end
 
@@ -20,42 +21,6 @@ module CodewarsApiRuby
 
     def _build_language_content(key, value)
       instance_variable_set("@#{key}", OpenStruct.new(value))
-    end
-
-    def _build_user(data)
-      data.each do |key, value|
-        if value.is_a? Hash
-          _build_methods(value)
-        else
-          _mount_values_and_methods(key, value)
-        end
-      end
-    end
-
-    def _build_methods(values)
-      values.each do |key, value|
-        _mount_values_and_methods(key, value)
-      end
-    end
-
-    def _mount_values_and_methods(key, value)
-      new_key = _underscore_key(key)
-      _build_attr_reader(new_key)
-      _set_attr_value(new_key, value)
-    end
-
-    def _build_attr_reader(key)
-      self.class.instance_eval("attr_reader :#{key}")
-    end
-
-    def _set_attr_value(key, value)
-      instance_variable_set("@#{key}", value)
-    end
-
-    def _underscore_key(key)
-      match = key.match(/^([a-z]*)([A-Z].*)/)
-      return key unless match
-      "#{match[1]}_#{match[2].downcase}"
     end
   end
 end
