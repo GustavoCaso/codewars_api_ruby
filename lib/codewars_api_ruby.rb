a6e12643faffcf26ef7daf4cd41443693aede21f
GustavoCaso <gustavocaso@gmail.com>
2015-12-30 18:24:35 +0100

    Test deferred_response and fetch_deferred_result

--- lib/codewars_api_ruby.rb
+++ lib/codewars_api_ruby.rb
@@ -5,9 +5,11 @@ require "codewars_api_ruby/user"
 require "codewars_api_ruby/client"
 require "codewars_api_ruby/configuration"
 require "codewars_api_ruby/version"
+require 'json'
 
 module CodewarsApiRuby
   class InvalidLanguageSelection < StandardError; end
+  class ToManyRequest < StandardError; end
   extend Configuration
   extend Client
 
@@ -36,16 +38,21 @@ module CodewarsApiRuby
     end
 
     def deferred_response(dmid:)
+      get(path: DeferredResponse::PATH, params: dmid)
+    end
+
+    def fetch_deferred_result(dmid:)
       fetching_result = true
       count = 1
       while fetching_result do
-        status, response = get(path: Deferred::PATH, params: dmid)
-        if response['success']
+        fail ToManyRequest if count > 10
+        staus , response = deferred_response(dmid: dmid)
+        result = JSON.parse(response)
+        if result['success']
           fetching_result = false
           return DeferredResponse.new(data: response)
         end
         count += 1
-        puts "Trying the fetch deferred inforamtion for #{dmid} count: #{count}"
         sleep 1
       end
     end
--- spec/codewars_api_ruby_spec.rb
+++ spec/codewars_api_ruby_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe CodewarsApiRuby do
+
+  let :next_kata_request do
+    VCR.use_cassette 'next_kata' do
+      described_class.next_kata(language: :ruby)
+    end
+  end
+
   context 'User' do
     let :user_request do
       VCR.use_cassette 'user' do
@@ -8,7 +15,7 @@ describe CodewarsApiRuby do
       end
     end
     subject { user_request }
-    it 'will return a valid user object' do
+    it 'will return a user object' do
       expect(subject).to be_a CodewarsApiRuby::User
     end
 
@@ -16,17 +23,48 @@ describe CodewarsApiRuby do
   end
 
   context 'NextKata' do
-    let :next_kata_request do
-      VCR.use_cassette 'next_kata' do
-        described_class.next_kata(language: :ruby)
-      end
-    end
-
     subject { next_kata_request }
-    it 'will return a valid next_kata object' do
+    it 'will return a next_kata object' do
       expect(next_kata_request).to be_a CodewarsApiRuby::NextKata
     end
 
     it_should_behave_like 'next_kata_methods'
+  end
+
+  context 'AttemptSolution' do
+    let(:kata) { next_kata_request }
+    let(:code) { File.read(File.expand_path('../fixtures/ruby_code_example.rb', __FILE__)) }
+    let(:attemp_solution_request) do
+      VCR.use_cassette 'attemp_solution' do
+        described_class.attemp_solution(kata: kata, code: code)
+      end
+    end
+    it 'should raise error if no code provided' do
+      expect{
+        described_class.attemp_solution(kata: kata)
+      }.to raise_error ArgumentError
+    end
+
+    it 'should return a attemp_solution object' do
+      expect(attemp_solution_request).to be_a CodewarsApiRuby::AttemptSolution
+    end
+
+    context 'DeferredResponse' do
+      subject { described_class.fetch_deferred_result(dmid: attemp_solution_request.dmid) }
+      it 'should raise error if to many request are made' do
+        VCR.use_cassette 'deferred_response' do
+          described_class.deferred_response(dmid: attemp_solution_request.dmid)
+          expect{
+            subject
+          }.to raise_error CodewarsApiRuby::ToManyRequest
+        end
+      end
+
+      it 'should return a deferred_response object if success' do
+        VCR.use_cassette 'deferred_response_valid', erb: true do
+          expect(subject).to be_a CodewarsApiRuby::DeferredResponse
+        end
+      end
+    end
   end
 end
