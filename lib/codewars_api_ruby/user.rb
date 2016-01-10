86efff50158e37903b693391380b5a0db62e6147
GustavoCaso <gustavocaso@gmail.com>
2016-01-11 00:37:27 +0100

    Simplify user object

--- lib/codewars_api_ruby/user.rb
+++ lib/codewars_api_ruby/user.rb
@@ -7,20 +7,6 @@ module CodewarsApiRuby
 
     def initialize(data:)
       _build_object(data)
-      _build_language_methods
-    end
-
-    private
-
-    def _build_language_methods
-      languages.each do |key, value|
-        _build_attr_reader(key)
-        _build_language_content(key, value)
-      end
-    end
-
-    def _build_language_content(key, value)
-      instance_variable_set("@#{key}", OpenStruct.new(value))
     end
   end
 end
